44b82f4198dc6b6246ed8a0cd002d14c0de5ae5b
Linh <nguyenthilinhqni2020@gmail.com>
2024-04-03 06:43:10 +0700

    Check authentication on a device

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Psy\VersionUpdater\Checker;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        return $request->expectsJson() ? null : route('login');
+    }
+    protected function authenticate($request, array $guards)
+    {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                // check logic
+                // lấy dduuoc session id hiện tại
+                // so sánh với sessionid trong bang user
+                // nếu khác nhau logout giong bỏ qua
+                $checkDevice=$this->checkDevice($request);
+                if(!$checkDevice){
+                    $this->unauthenticated($request, $guards);
+                }
+                return $this->auth->shouldUse($guard);
+            }
+        }
+
+        $this->unauthenticated($request, $guards);
+    }
+    private function checkDevice($request){
+        $sessionId = $request->session()->getId();
+        $user =$request ->user();
+        $lastSessionId=$user->last_session;
+        if($lastSessionId !== $sessionId){
+            Auth::logout();
+            return  false;
+        }
+        
+        return true;
+    }
+}
